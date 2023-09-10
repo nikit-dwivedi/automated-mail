ec934fb537046999549c3e6b4752d81f95a98efc
nikitdwivedi <nikitdwivedi1997@gmail.com>
2023-09-10 19:39:05 +0530

    https server

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const http = require('http')
+const https = require('https')
+var fs = require('fs');
 const app = require('./app');
 require("dotenv").config()
 
 
+var privateKey = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+let credentials = { key: privateKey, cert: certificate };
+
 const server = http.createServer(app)
+const secureServer = https.createServer(credentials, app);
 
 const port = process.env.PORT
+const sPort = process.env.SPORT
 
-
-server.listen(port,  (err) => {
+secureServer.listen(sPort, () => {
+    console.log(`Secure server listening at ${sPort}`);
+})
+server.listen(port, (err) => {
     if (err) {
         console.log(err);
     } else {
